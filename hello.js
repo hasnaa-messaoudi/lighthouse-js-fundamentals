e80b30b40dcaef517a418d5fe4ee6731cc2c4144
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-07-28 21:53:39 +0000

    add hello exercice

---
+++ hello.js
@@ -0,0 +1,15 @@
+const sayHello  = function () {
+    console.log("Hello, world");
+}
+  
+const sayHello2  = function (name) {
+    console.log("Hello, " + name);
+}
+  
+
+sayHello();
+
+sayHello2("Hasnaa");
+sayHello2("Caliban");
+sayHello2("Miranda");
+sayHello2("Ferdinand");
