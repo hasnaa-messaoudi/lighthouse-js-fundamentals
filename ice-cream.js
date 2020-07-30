1201de001debc818eea12aa6b8da18a91f1fa78b
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-07-30 13:50:29 +0000

    add ice-cream challenge

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+
+// Your code below here...
+let iceCreamFlavoursNew = iceCreamFlavours;
+iceCreamFlavoursNew.push("root beer");
+console.log(iceCreamFlavoursNew);
+console.log(iceCreamFlavoursNew[0]);
+console.log(iceCreamFlavoursNew[iceCreamFlavoursNew.length - 1]);
+console.log(iceCreamFlavoursNew.length);
